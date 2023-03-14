27ada6020efa1ec6e8200c849bd64878cbd72c05
Nhoj <amosinnhoj@gmail.com>
2023-03-14 20:49:53 +0800

    doing iteration 4

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,5 @@
 require 'csv'
-puts 'Event Manager Initialized!'
+require 'google/apis/civicinfo_v2'
 
 #contents = File.read('event_attenndees.csv')
 #puts contents
@@ -51,8 +51,7 @@ puts 'Event Manager Initialized!'
 #end
 
 
-# Parsing with csv ITERATION 2
-
+## Parsing with csv ITERATION 2
 #def clean_zipcode(zipcode)
 #    if zipcode.nil?
 #      '00000'
@@ -94,4 +93,60 @@ puts 'Event Manager Initialized!'
 #end
 
 
-# Using Google's civic information ITERATION 3
+## Using Google's civic information ITERATION 3
+#def legislator_by_zipcode(zip)
+#  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+#  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+#
+#  begin
+#    legislators = civic_info.representative_info_by_address(
+#      address: zip,
+#      levels: 'country',
+#      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+#    )
+#    legislators = legislators.officials
+#    legislator_names = legislators.map(&:name)
+#    legislator_names.join(", ")
+#  rescue
+#    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+#  end
+#end
+#
+#def clean_zipcode(zipcode)
+#  zipcode.to_s.rjust(5, '0')[0..4]
+#end
+#
+#puts 'EventManager initialized.'
+#
+#contents = CSV.open(
+#  'event_attendees.csv',
+#  headers: true,
+#  header_converters: :symbol
+#)
+#
+#contents.each do |row|
+#  name = row[:first_name]
+#
+#  zipcode = clean_zipcode(row[:zipcode])
+#
+#  legislators = legislator_by_zipcode(zipcode)
+#
+#  puts "#{name} #{zipcode} #{legislators}"
+#end
+
+## Iteration 4: Form letters
+## %{} is one choice when defining a string that spans mutiple lines
+template_letter = File.read('form_letter.html')
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+
+  puts personal_letter
+end
