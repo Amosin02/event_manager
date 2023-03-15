18053f92fd87216de83b74acc7955c3ff3079c74
Nhoj <amosinnhoj@gmail.com>
2023-03-15 20:35:22 +0800

    doing the assignments

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 #contents = File.read('event_attenndees.csv')
 #puts contents
@@ -94,7 +95,7 @@ require 'google/apis/civicinfo_v2'
 
 
 ## Using Google's civic information ITERATION 3
-#def legislator_by_zipcode(zip)
+#def legislators_by_zipcode(zip)
 #  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
 #  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 #
@@ -136,17 +137,79 @@ require 'google/apis/civicinfo_v2'
 
 ## Iteration 4: Form letters
 ## %{} is one choice when defining a string that spans mutiple lines
-template_letter = File.read('form_letter.html')
+#puts "EventManager initialized."
+#template_letter = File.read('form_letter.html')
+
+#contents = CSV.open(
+#  'event_attendees.csv',
+#  headers: true,
+#  header_converters: :symbol
+#)
+
+#contents.each do |row|
+#  name = row[:first_name]
+#
+#  zipcode = clean_zipcode(row[:zipcode])
+#
+#  legislators = legislators_by_zipcode(zipcode)
+#
+#  personal_letter = template_letter.gsub('FIRST_NAME', name)
+#  personal_letter.gsub!('LEGISLATORS', legislators)
+#
+#  #puts personal_letter
+#end
+
+## ----------------------------------------------------
+
+puts "EventManager initialized."
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
+  form_letter = erb_template.result(binding)
 
-  puts personal_letter
+  save_thank_you_letter(id, form_letter)
 end
+
+##----------------------------------------------------------------
